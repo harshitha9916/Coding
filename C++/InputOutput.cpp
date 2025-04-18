4b69ad9def73fd3167289269c54d0c3eee91f39a
Harshitha Diddi <harshitha1579@gmail.com>
2025-04-17 22:52:52 -0400

    add First C++ program

---
+++ C++/InputOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <math.h>
+//#include <string>
+using namespace std;
+
+int main(){
+    cout << "Hi"<< "\n";
+    cout << "Harshi" << endl;
+    cout << "lu" ;
+    int x;
+    cin >> x;
+    cout << "value of x is " << x;
+    return 0;
+
+}
